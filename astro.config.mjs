b0b67aead37ab90c4818fd32f3f9177618f43313
Danila Kononov <danila.kononov.official@gmail.com>
2025-08-25 23:32:23 +0300

    initial commit

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,5 +1,21 @@
 // @ts-check
+import tailwindcss from '@tailwindcss/vite';
 import { defineConfig } from 'astro/config';
+import sitemap from '@astrojs/sitemap';
 
 // https://astro.build/config
-export default defineConfig({});
+export default defineConfig({
+    site: 'https://nncast.ru',
+    vite: {
+        plugins: [tailwindcss()],
+    },
+    integrations: [
+        sitemap(),
+    ],
+    build: {
+        inlineStylesheets: "always",
+    },
+    devToolbar: {
+        enabled: false,
+    },
+});
